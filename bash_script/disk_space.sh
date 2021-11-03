7b33b051cffdece05536edd69d791e91318c08e2
sathish-cs <sathishtech88@gmail.com>
2021-11-03 12:51:57 +0530

    Updated fix

--- bash_script/disk_space.sh
+++ bash_script/disk_space.sh
@@ -1,13 +1,16 @@
 
 #!/bin/sh
-SUBJECT="Disk Usage Report on "`date`""
+DISK_USAGE="Disk Usage Report on "`date`""
 MESSAGE="/tmp/disk-usage.out"
 MESSAGE1="/tmp/disk-usage-1.out"
 echo "---------------------------------------------------------------------------------------------------" >> $MESSAGE1
 echo "HostName            Filesystem                Size Used Avail Use% Mounted on" >> $MESSAGE1
 echo "---------------------------------------------------------------------------------------------------" >> $MESSAGE1
-for server in `more ~/Documents/servers.txt`
+# Using for loop to ssh multiple servers
+for server in `more ./servers.txt` 
 do
+# SSH to servers and check for the disk usage under /home directory
 output=`ssh $server  du -hsx /home/ | sort -rh | head -10`
+# Redirecting server list and output to a file
 echo "$server: $output" >> $MESSAGE
 done
